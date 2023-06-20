855c3ee411d988fed383b2c8c5f2fc42cf748302
Elliot Galibert <elliotgalibert@gmail.com>
2023-06-20 10:27:54 +0300

    started today

---
+++ Part2/SquareRootOfSum.java
@@ -0,0 +1,16 @@
+package Java_Programming.Java_Programmin_1_Helsinki_University.Part2;
+
+import java.util.Scanner;
+
+public class SquareRootOfSum {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = Integer.valueOf(scanner.nextLine());
+        int num2 = Integer.valueOf(scanner.nextLine());
+
+        double SquareRootOfSum = Math.sqrt(num + num2);
+        System.out.println(SquareRootOfSum);
+
+    }
+}
---
+++ Part2/Squared.java
@@ -0,0 +1,14 @@
+package Java_Programming.Java_Programmin_1_Helsinki_University.Part2;
+
+import java.util.Scanner;
+
+public class Squared {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = Integer.valueOf(scanner.nextLine());
+
+        System.out.println(num * num);
+
+    }
+}
