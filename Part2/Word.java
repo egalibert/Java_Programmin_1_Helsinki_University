5af6681b74848360f33b0254a42a4c3aab8e1bfc
Elliot Galibert <elliotgalibert@gmail.com>
2023-06-27 11:06:28 +0300

    started todays exes

---
+++ Part2/Word.java
@@ -0,0 +1,16 @@
+package Java_Programming.Java_Programmin_1_Helsinki_University.Part2;
+
+import java.util.Scanner;
+
+public class Word {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(word());
+
+    }
+	public static String word() {
+		return "Seewy!";
+	}
+
+}
