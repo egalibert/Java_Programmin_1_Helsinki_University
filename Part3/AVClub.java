e0b8b068069281a1ed76513e9bd620ecd6a09462
Elliot Galibert <elliotgalibert@gmail.com>
2023-06-29 19:14:27 +0300

    five left

---
+++ Part3/AVClub.java
@@ -0,0 +1,23 @@
+package Java_Programming.Java_Programmin_1_Helsinki_University.Part3;
+
+import java.util.Scanner;
+
+public class AVClub {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String text = scanner.nextLine();
+            if (text.equals("")) {
+                break;
+            }
+            String[] pieces = text.split(" ");
+            for (int i = 0; i < pieces.length; i++) {
+                if (pieces[i].contains("av")) {
+                    System.out.println(pieces[i]);
+                }
+            }
+        }
+    }
+}
